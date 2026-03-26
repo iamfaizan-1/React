4e61906126d0fc2bccf9d1681eca822fb538e232
iamfaizan-1 <faizanfaizy634@gmail.com>
2026-03-26 08:43:51 +0500

    react router

---
+++ react router/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+
+      <h1 className='text-3xl font-bold' >This is app.jsx</h1>
+
+    
+    </div>
+  )
+}
+
+export default App
